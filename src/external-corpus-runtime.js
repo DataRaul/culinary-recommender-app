610469400a7bfcc63df51d8e637f0dfed327567b
Raul M. <67912886+DataRaul@users.noreply.github.com>
2026-09-01 00:33:10 +0100

    Attach normalized external RecipeSource records at runtime

--- src/external-corpus-runtime.js
+++ src/external-corpus-runtime.js
@@ -1,9 +1,8 @@
 import { RECIPES } from "./data/recipes.js";
-import "./data/corpus-v1.js";
-import { WIKIBOOKS_GATE_F_RECIPES } from "./data/external/wikibooks-gate-f-v1.js";
+import { EXTERNAL_RECIPES } from "./data/corpus-v1.js";
 
 const existingIds = new Set(RECIPES.map(recipe => recipe.id));
-for (const recipe of WIKIBOOKS_GATE_F_RECIPES) {
+for (const recipe of EXTERNAL_RECIPES) {
   if (existingIds.has(recipe.id)) continue;
   RECIPES.push(recipe);
   existingIds.add(recipe.id);
