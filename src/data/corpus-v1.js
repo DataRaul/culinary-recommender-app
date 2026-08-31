b45f17d04abc96d9bc97888508f6b05196dc631b
Raul M. <67912886+DataRaul@users.noreply.github.com>
2026-09-01 00:29:47 +0100

    Combine authored and external recipes behind RecipeSource

--- src/data/corpus-v1.js
+++ src/data/corpus-v1.js
@@ -1,6 +1,7 @@
 import { RECIPES } from "./recipes.js";
 import { EXPANDED_RECIPES } from "./recipes-v1.js";
 import { SEARCH_COVERAGE_RECIPES } from "./recipes-v1-search.js";
+import { WIKIBOOKS_GATE_F_RECIPES } from "./external/wikibooks-gate-f-v1.js";
 
 const existingIds = new Set(RECIPES.map(recipe => recipe.id));
 for (const recipe of [...EXPANDED_RECIPES, ...SEARCH_COVERAGE_RECIPES]) {
@@ -10,5 +11,15 @@ for (const recipe of [...EXPANDED_RECIPES, ...SEARCH_COVERAGE_RECIPES]) {
   }
 }
 
-export const ALL_RECIPES = RECIPES;
+export const AUTHORED_RECIPES = RECIPES;
+export const EXTERNAL_RECIPES = WIKIBOOKS_GATE_F_RECIPES;
+
+const universeIds = new Set(AUTHORED_RECIPES.map(recipe => recipe.id));
+const externalWithoutIdCollisions = EXTERNAL_RECIPES.filter(recipe => {
+  if (universeIds.has(recipe.id)) return false;
+  universeIds.add(recipe.id);
+  return true;
+});
+
+export const ALL_RECIPES = Object.freeze([...AUTHORED_RECIPES, ...externalWithoutIdCollisions]);
 export const recipeByIdV1 = id => ALL_RECIPES.find(recipe => recipe.id === id) || null;
